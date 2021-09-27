9e8477b611fd1988cf85c007c999c8f948b5a253
Glitch (a3-persistence-maxine-shi) <none>
2021-09-27 07:14:42 +0000

    formated with Glitch

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,30 @@
-
-const getUrgency = function (expiraton) {
+const getUrgency = function(expiraton) {
   let urgency = "";
-   
+
   if (expiraton === "expiration-long") {
     urgency = "Take Your Time";
   } else if (expiraton === "expiration-med") {
     urgency = "Finish it Soon";
   } else if (expiraton === "expiration-short") {
     urgency = "Finish it Now";
-  } else  {
+  } else {
     urgency = "Finish it Soon";
   }
   return urgency;
-}
+};
 
-const addEntry = (json) => {
+const addEntry = json => {
+  let table = document.getElementById("items");
 
-  let table = document.getElementById('items')
-  
   let newRow = table.insertRow(-1),
-  newItem = newRow.insertCell(0),
-  newDescription = newRow.insertCell(1),
-  newExpiration = newRow.insertCell(2),
-  newUrgency = newRow.insertCell(3),
-  editElement = newRow.insertCell(4),
-  editButton = document.createElement('Input'),
-  removeElement = newRow.insertCell(5),
-  removeButton = document.createElement('Input');
+    newItem = newRow.insertCell(0),
+    newDescription = newRow.insertCell(1),
+    newExpiration = newRow.insertCell(2),
+    newUrgency = newRow.insertCell(3),
+    editElement = newRow.insertCell(4),
+    editButton = document.createElement("Input"),
+    removeElement = newRow.insertCell(5),
+    removeButton = document.createElement("Input");
 
   let idElement = json._id;
 
@@ -35,78 +33,82 @@ const addEntry = (json) => {
   newExpiration.innerHTML = json.expiraton;
   newUrgency.innerHTML = json.urgency;
 
-  editButton.setAttribute('type','button')
-  editButton.setAttribute('value', 'Edit')
-  editButton.className = 'editButton';
+  editButton.setAttribute("type", "button");
+  editButton.setAttribute("value", "Edit");
+  editButton.className = "editButton";
   editElement.appendChild(editButton);
 
-  let itemInput = document.createElement('Input'),
-    descriptionInput = document.createElement('Input'),
-    expirationInput = document.createElement('Input'),
-    urgencyInput = document.createElement('Input'),
+  let itemInput = document.createElement("Input"),
+    descriptionInput = document.createElement("Input"),
+    expirationInput = document.createElement("Input"),
+    urgencyInput = document.createElement("Input"),
     id = idElement;
 
-    itemInput.value = newItem.innerHTML;
-    descriptionInput.value = newDescription.innerHTML;
-    expirationInput.value = newExpiration.innerHTML;
-    urgencyInput.value = urgencyInput.innerHTML;
+  itemInput.value = newItem.innerHTML;
+  descriptionInput.value = newDescription.innerHTML;
+  expirationInput.value = newExpiration.innerHTML;
+  urgencyInput.value = urgencyInput.innerHTML;
 
   editButton.onclick = function() {
-
-    document.querySelector('#itemName').value = json.itemName
-    document.querySelector('#itemDescription').value = json.description
-    document.querySelector('#expiration').value = json.expiration
+    document.querySelector("#itemName").value = json.itemName;
+    document.querySelector("#itemDescription").value = json.description;
+    document.querySelector("#expiration").value = json.expiration;
 
     newRow.remove();
 
-    json = { itemName: itemInput.value, description: descriptionInput.value, expiration: expirationInput.value, urgency: urgencyInput.value, id: id }
-    body = JSON.stringify( json )
+    json = {
+      itemName: itemInput.value,
+      description: descriptionInput.value,
+      expiration: expirationInput.value,
+      urgency: urgencyInput.value,
+      id: id
+    };
+    body = JSON.stringify(json);
 
-    fetch('/update', {
-      method:'POST',
+    fetch("/update", {
+      method: "POST",
       body,
-      headers: {"Content-Type": "application/json"} 
+      headers: { "Content-Type": "application/json" }
     })
-    .then(response => response.json())
-    .then(json => {
-      
-      itemInput.value = newItem.innerHTML
-      descriptionInput.value = newDescription.innerHTML
-      expirationInput.value = newExpiration.innerHTML
-      urgencyInput.value = newUrgency.innerHTML
-    })
-  }
+      .then(response => response.json())
+      .then(json => {
+        itemInput.value = newItem.innerHTML;
+        descriptionInput.value = newDescription.innerHTML;
+        expirationInput.value = newExpiration.innerHTML;
+        urgencyInput.value = newUrgency.innerHTML;
+      });
+  };
 
-  removeButton.setAttribute('type','button')
-  removeButton.setAttribute('value', 'Remove')
-  removeButton.classItem = 'removeButton';
+  removeButton.setAttribute("type", "button");
+  removeButton.setAttribute("value", "Remove");
+  removeButton.classItem = "removeButton";
   removeElement.appendChild(removeButton);
 
-  removeButton.onclick = function () {
-
-    fetch( '/remove', {
-      method:'POST',
-      body: JSON.stringify({idElement}),
-      headers: {"Content-Type": "application/json"} 
+  removeButton.onclick = function() {
+    fetch("/remove", {
+      method: "POST",
+      body: JSON.stringify({ idElement }),
+      headers: { "Content-Type": "application/json" }
     })
-    .then(response => response.json())
-    .then(json => {newRow.remove();
-   })
-  }
+      .then(response => response.json())
+      .then(json => {
+        newRow.remove();
+      });
+  };
 };
 
 function loadDatabase(item) {
-  let table = document.getElementById('items')
-  
+  let table = document.getElementById("items");
+
   let newRow = table.insertRow(-1),
-  newItem = newRow.insertCell(0),
-  newDescription = newRow.insertCell(1),
-  newExpiration = newRow.insertCell(2),
-  newUrgency = newRow.insertCell(3),
-  editElement = newRow.insertCell(4),
-  editButton = document.createElement('Input'),
-  removeElement = newRow.insertCell(5),
-  removeButton = document.createElement('Input');
+    newItem = newRow.insertCell(0),
+    newDescription = newRow.insertCell(1),
+    newExpiration = newRow.insertCell(2),
+    newUrgency = newRow.insertCell(3),
+    editElement = newRow.insertCell(4),
+    editButton = document.createElement("Input"),
+    removeElement = newRow.insertCell(5),
+    removeButton = document.createElement("Input");
 
   let idElement = item._id;
 
@@ -115,129 +117,140 @@ function loadDatabase(item) {
   newExpiration.innerHTML = item.expiraton;
   newUrgency.innerHTML = item.urgency;
 
-  editButton.setAttribute('type','button')
-  editButton.setAttribute('value', 'Edit')
-  editButton.className = 'editButton';
+  editButton.setAttribute("type", "button");
+  editButton.setAttribute("value", "Edit");
+  editButton.className = "editButton";
   editElement.appendChild(editButton);
 
-  let itemInput = document.createElement('Input'),
-    descriptionInput = document.createElement('Input'),
-    expirationInput = document.createElement('Input'),
-    urgencyInput = document.createElement('Input'),
+  let itemInput = document.createElement("Input"),
+    descriptionInput = document.createElement("Input"),
+    expirationInput = document.createElement("Input"),
+    urgencyInput = document.createElement("Input"),
     id = idElement;
 
-    itemInput.value = newItem.innerHTML;
-    descriptionInput.value = newDescription.innerHTML;
-    expirationInput.value = newExpiration.innerHTML;
-    urgencyInput.value = urgencyInput.innerHTML;
+  itemInput.value = newItem.innerHTML;
+  descriptionInput.value = newDescription.innerHTML;
+  expirationInput.value = newExpiration.innerHTML;
+  urgencyInput.value = urgencyInput.innerHTML;
 
   editButton.onclick = function() {
-
-    document.querySelector('#itemName').value = item.itemName
-    document.querySelector('#itemDescription').value = item.description
-    document.querySelector('#expiration').value = item.expiration
+    document.querySelector("#itemName").value = item.itemName;
+    document.querySelector("#itemDescription").value = item.description;
+    document.querySelector("#expiration").value = item.expiration;
 
     newRow.remove();
 
-    json = { itemName: itemInput.value, description: descriptionInput.value, expiration: expirationInput.value, urgency: urgencyInput.value, id: id }
-    body = JSON.stringify( json )
+    json = {
+      itemName: itemInput.value,
+      description: descriptionInput.value,
+      expiration: expirationInput.value,
+      urgency: urgencyInput.value,
+      id: id
+    };
+    body = JSON.stringify(json);
 
-    fetch( '/update', {
-      method:'POST',
+    fetch("/update", {
+      method: "POST",
       body,
-      headers: {"Content-Type": "application/json"} 
+      headers: { "Content-Type": "application/json" }
     })
-    .then(response => response.json())
-    .then(json => {
-      
-      itemInput.value = newItem.innerHTML
-      descriptionInput.value = newDescription.innerHTML
-      expirationInput.value = newExpiration.innerHTML
-      urgencyInput.value = newUrgency.innerHTML
-    })
-  }
+      .then(response => response.json())
+      .then(json => {
+        itemInput.value = newItem.innerHTML;
+        descriptionInput.value = newDescription.innerHTML;
+        expirationInput.value = newExpiration.innerHTML;
+        urgencyInput.value = newUrgency.innerHTML;
+      });
+  };
 
-  removeButton.setAttribute('type','button')
-  removeButton.setAttribute('value', 'Remove')
-  removeButton.className = 'removeButton';
+  removeButton.setAttribute("type", "button");
+  removeButton.setAttribute("value", "Remove");
+  removeButton.className = "removeButton";
   removeElement.appendChild(removeButton);
 
-
-  removeButton.onclick = function () {
-
-    fetch( '/remove', {
-      method:'POST',
-      body: JSON.stringify({idElement}),
-      headers: {"Content-Type": "application/json"} 
+  removeButton.onclick = function() {
+    fetch("/remove", {
+      method: "POST",
+      body: JSON.stringify({ idElement }),
+      headers: { "Content-Type": "application/json" }
     })
-    .then(response => response.json())
-    .then(json => {newRow.remove()})
-  }
+      .then(response => response.json())
+      .then(json => {
+        newRow.remove();
+      });
+  };
 }
 
 fetch("/fridge")
-.then(response => response.json())
-.then(fridge => {fridge.forEach(loadDatabase);});
+  .then(response => response.json())
+  .then(fridge => {
+    fridge.forEach(loadDatabase);
+  });
 
-const submit = function( e ) {
+const submit = function(e) {
+  e.preventDefault();
 
-  e.preventDefault()
-
-    const item = document.querySelector( '#itemName' ),
-    description = document.querySelector( '#itemDescription' ),
-    expiration = document.querySelector( '#expiration' ),
+  const item = document.querySelector("#itemName"),
+    description = document.querySelector("#itemDescription"),
+    expiration = document.querySelector("#expiration"),
     urgency = getUrgency(expiration.value);
 
-    if (item.value === "" || expiration.value === ""||description.value === "") {
-        console.log("Crucial fields are empty")
-        alert("Item and expiration fields needs to be filled.")
-        return false
-    } 
+  if (
+    item.value === "" ||
+    expiration.value === "" ||
+    description.value === ""
+  ) {
+    console.log("Crucial fields are empty");
+    alert("Item and expiration fields needs to be filled.");
+    return false;
+  }
 
+  if (item.value.length < 2 || item.value.length < 2) {
+    console.log("Not a valid item or description");
+    alert("Enter valid fields only.");
+    return false;
+  }
 
-    if (item.value.length < 2 ||item.value.length < 2 ) {
-      console.log("Not a valid item or description")
-      alert("Enter valid fields only.")
-      return false
-    }
+  if (item.value === "Item") {
+    console.log("Not valid item");
+    alert("Enter only valid fields for item.");
+    return false;
+  }
 
-   
-    if (item.value === "Item" ) {
-        console.log("Not valid item")
-        alert("Enter only valid fields for item.")
-        return false
-    }
-  
-    json = { itemName: item.value, description: description.value, expiraton:expiration.value, urgency: urgency }
-    body = JSON.stringify( json )
+  json = {
+    itemName: item.value,
+    description: description.value,
+    expiraton: expiration.value,
+    urgency: urgency
+  };
+  body = JSON.stringify(json);
 
-    fetch( '/submit', {
-      method:'POST',
-      body: body,
-      headers: {"Content-Type": "application/json"} 
-    })
+  fetch("/submit", {
+    method: "POST",
+    body: body,
+    headers: { "Content-Type": "application/json" }
+  })
     .then(response => response.json())
-    .then(json => {addEntry(json)})
-    return false
-  }
+    .then(json => {
+      addEntry(json);
+    });
+  return false;
+};
 
-  
-  const logout = function (e) {
+const logout = function(e) {
+  e.preventDefault();
 
-      e.preventDefault()
+  fetch("logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" }
+  });
+  window.location.href = "login.html";
+};
 
-      fetch('logout', {
-          method: 'POST',
-          headers: {"Content-Type":"application/json"}
-      })
-      window.location.href = "login.html"
-  }
-  
-  window.onload = function() {
-    const addButton = document.querySelector("#addItem");
-    addButton.onclick = submit;
-  
+window.onload = function() {
+  const addButton = document.querySelector("#addItem");
+  addButton.onclick = submit;
 
-    const logoutButton = document.getElementById('logoutBtn')
-    logoutButton.onclick = logout
-  }
+  const logoutButton = document.getElementById("logoutBtn");
+  logoutButton.onclick = logout;
+};
--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,23 @@
-const login = function (e) {
-    
-    e.preventDefault()
+const login = function(e) {
+  e.preventDefault();
 
-    const username = document.querySelector('#username'),
-    password = document.querySelector('#password');
+  const username = document.querySelector("#username"),
+    password = document.querySelector("#password");
 
-    //Error checking that the user enters both fields
-    if (username.value === "" || password.value === "") {
-        alert("Enter a valid username and password");
-        return false
-    }
+  //Error checking that the user enters both fields
+  if (username.value === "" || password.value === "") {
+    alert("Enter a valid username and password");
+    return false;
+  }
 
-    json = {username: username.value, password: password.value}
-    body = JSON.stringify(json);
+  json = { username: username.value, password: password.value };
+  body = JSON.stringify(json);
 
-    fetch( '/login', {
-        method: 'POST',
-        body: body, 
-        headers: {"Content-Type": "application/json"}
-    })
+  fetch("/login", {
+    method: "POST",
+    body: body,
+    headers: { "Content-Type": "application/json" }
+  })
     .then(response => response.json())
     .then(user => {
       if (user.length == 0) {
@@ -27,39 +26,36 @@ const login = function (e) {
         window.location.href = "index.html";
       }
     });
-    return false
-}
+  return false;
+};
 
-const signup = function (e) {
+const signup = function(e) {
+  e.preventDefault();
 
-    e.preventDefault()
+  const username = document.getElementById("username"),
+    password = document.getElementById("password");
 
-    const username = document.getElementById('username'),
-    password = document.getElementById('password');
+  //Error checking that the user signs-up with valid fields
+  if (username.value === "" || password.value === "") {
+    alert("Enter a valid username and password");
+    return false;
+  }
 
-    //Error checking that the user signs-up with valid fields
-    if (username.value === "" || password.value === "") {
-        alert("Enter a valid username and password");
-        return false
-    }
+  json = { username: username.value, password: password.value };
+  body = JSON.stringify(json);
 
-    json = {username: username.value, password: password.value}
-    body = JSON.stringify(json)
-
-    fetch('/signin', {
-        method: 'POST',
-        body: body,
-        headers: {"Content-Type" : "application/json"}
-    })
-    .then(response => response.json())
-    window.location.href = "index.html"
-}
+  fetch("/signin", {
+    method: "POST",
+    body: body,
+    headers: { "Content-Type": "application/json" }
+  }).then(response => response.json());
+  window.location.href = "index.html";
+};
 
 window.onload = function() {
+  const loginBtn = document.getElementById("loginBtn");
+  loginBtn.onclick = login;
 
-    const loginBtn = document.getElementById('loginBtn');
-    loginBtn.onclick = login;
-
-    const signUpBtn = document.getElementById('signUp')
-    signUpBtn.onclick = signup;
-}
+  const signUpBtn = document.getElementById("signUp");
+  signUpBtn.onclick = signup;
+};
